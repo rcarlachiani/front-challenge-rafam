524828c50537da8a51618e7e73b6ac67e3a42535
Rodrigo Carlachiani <rodrigo@genosha.com.ar>
2023-07-12 16:36:40 -0300

    styled sidebar and added dropdown list - still working on it

--- src/components/atoms/DropdownSidebar/index.js
+++ src/components/atoms/DropdownSidebar/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.scss';
 
 const DropdownSidebar = () => {
+    const [toggleArrow, setToggleArrow] = useState(undefined);
     const menuItems = [
         {key: "item1", title: "Presupuesto"},
         {key: "item2", title: "CAS"},
@@ -14,27 +15,55 @@ const DropdownSidebar = () => {
         {key: "item9",title: "Inversión Pública"},
         {key: "item10",title: "Crédito Público"},
         {key: "item11",title: "Recursos Humanos"},
-    ]
+    ];
+    const menuSubItems1 = [
+        {key: "subItem1", title: "Configuración"},
+        {key: "subItem2", title: "Formulación presupuestaria"},
+        {key: "subItem3", title: "Modificación presupuestaria"},
+        {key: "subItem4", title: "Programación Física"},
+        {key: "subItem5", title: "Reportes"},
+    ];
 
-    return <div>
-        <ul className="dropdown-menu-list"> 
+    return <ul className="dropdown-menu-list"> 
             {menuItems.map((item) => (
                 <>
                     <li 
                         key={item.key}
                         className="dropdown-menu-item"
                     >
-                        <div className="dropdown-item-icon">
-                            <svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.22504 6.76923L0.906287 12.6316L0.906287 0.906905L9.22504 6.76923Z" fill="white"/>
-                            </svg>
+                        <div className="dropdown-item-container">
+                            <div className="dropdown-item-icon" onClick={() => setToggleArrow(toggleArrow === item.key ? undefined : item.key)}>
+                                <svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g className="svg-item-icon" transform={toggleArrow === item.key ? "rotate(90 5 6.5)" : "rotate(0)"}>
+                                        <path 
+                                            d="M9.22504 6.76923L0.906287 12.6316L0.906287 0.906905L9.22504 6.76923Z" 
+                                            fill="white"
+                                        />
+                                    </g>
+                                </svg>
+                            </div>
+                            <p 
+                                className="dropdown-item-text"
+                                onClick={() => setToggleArrow(toggleArrow === item.key ? undefined : item.key)}>
+                                {item.title}
+                            </p>
                         </div>
-                        {item.title}
+                        <ul className="dropdown-subMenu-list">
+                            {menuSubItems1.map((subitem) => (
+                                <li 
+                                    key={subitem.key}
+                                    className="dropdown-subMenu-item"
+                                >
+                                    <p className="dropdown-subMenu-item-text">
+                                        {subitem.title}
+                                    </p>
+                                </li>
+                            ))}
+                        </ul>
                     </li>
                 </>
             ))}
         </ul>
-    </div>
 }
 
 export default DropdownSidebar;
