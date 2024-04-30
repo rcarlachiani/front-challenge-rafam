1f32a299fab81c428b485cffd38c873ac439e7d4
Rodrigo Carlachiani <rcarlachiani@gmail.com>
2024-04-30 10:44:24 -0300

    Changed logos, app password and readme

---
+++ build/precache-manifest.dc5a9a330374ce5855352492ef0a549c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ed39868878d4836a2031069b24c840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51181669bab4c55bfce6",
+    "url": "/static/css/main.3a57b444.chunk.css"
+  },
+  {
+    "revision": "aac4738a89586b7a1d67",
+    "url": "/static/js/2.dbeb92b7.chunk.js"
+  },
+  {
+    "revision": "1ee0c6b92e11389f4be9768011524474",
+    "url": "/static/js/2.dbeb92b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51181669bab4c55bfce6",
+    "url": "/static/js/main.3afecabc.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.3afecabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1ad7584f23550d6be5",
+    "url": "/static/js/runtime-main.caae9abb.js"
+  },
+  {
+    "revision": "8a12323a9232c22466c316fe5cb76f6a",
+    "url": "/static/media/Mada-Black.8a12323a.ttf"
+  },
+  {
+    "revision": "e6cb5c6240e7775a445f5043d9aa1ce9",
+    "url": "/static/media/Mada-Bold.e6cb5c62.ttf"
+  },
+  {
+    "revision": "3227f63be2751a9f9785c7fff981a245",
+    "url": "/static/media/Mada-Light.3227f63b.ttf"
+  },
+  {
+    "revision": "594ec29a955c1d2c7856c1d383edb8b8",
+    "url": "/static/media/Mada-Medium.594ec29a.ttf"
+  },
+  {
+    "revision": "1a9388968c8d901b53cf06ba89d89962",
+    "url": "/static/media/Mada-Regular.1a938896.ttf"
+  }
+]);
