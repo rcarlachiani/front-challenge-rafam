793f20ef582507e27e4ec416031e3e041fa0b1e4
Rodrigo Carlachiani <rcarlachiani@gmail.com>
2024-04-30 10:44:24 -0300

    Changed logos, app password and readme

--- src/components/atoms/DropdownSelect/index.js
+++ src/components/atoms/DropdownSelect/index.js
@@ -8,10 +8,10 @@ function DropdownSelect() {
       aria-label='Default select example'
       className='dropdown-select'
     >
-      <option>2020</option>
-      <option value='2019'>2019</option>
-      <option value='2018'>2018</option>
-      <option value='2017'>2017</option>
+      <option>2024</option>
+      <option value='2023'>2023</option>
+      <option value='2022'>2022</option>
+      <option value='2021'>2021</option>
     </Form.Select>
   );
 }
